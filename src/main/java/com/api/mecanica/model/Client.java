7fe8ca887ed17d67ab819aebcca6db03c49fd1f2
danielsilvalima1996 <dsl15021996@gmail.com>
2021-11-18 20:20:49 -0300

    - ajustado client para telefone

--- src/main/java/com/api/mecanica/model/Client.java
+++ src/main/java/com/api/mecanica/model/Client.java
@@ -56,6 +56,15 @@ public class Client implements Serializable {
     )
     private Set<Address> address = new HashSet<Address>();
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @NotEmpty
+    @JoinTable(
+        name = "tb_client_phone", 
+        joinColumns = @JoinColumn(name = "client_id"), 
+        inverseJoinColumns = @JoinColumn(name = "phone_id")
+    )
+    private Set<Phone> phone = new HashSet<Phone>();
+
     @CreatedDate
 	@Column(name = "created", updatable = false)
 	private Date created;
@@ -75,11 +84,12 @@ public class Client implements Serializable {
     public Client() {
     }
 
-    public Client(Long id, String fullName, String cpfCnpj, Set<Address> address) {
+    public Client(Long id, String fullName, String cpfCnpj, Set<Address> address, Set<Phone> phone) {
         this.id = id;
         this.fullName = fullName;
         this.cpfCnpj = cpfCnpj;
         this.address = address;
+        this.phone = phone;
     }
 
     public Long getId() {
@@ -112,6 +122,14 @@ public class Client implements Serializable {
 
     public void setAddress(Set<Address> address) {
         this.address = address;
+    }
+
+    public Set<Phone> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Set<Phone> phone) {
+        this.phone = phone;
     }
 
     public Date getCreated() {
@@ -150,14 +168,7 @@ public class Client implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((cpfCnpj == null) ? 0 : cpfCnpj.hashCode());
-        result = prime * result + ((created == null) ? 0 : created.hashCode());
-        result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
-        result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((modified == null) ? 0 : modified.hashCode());
-        result = prime * result + ((modifiedBy == null) ? 0 : modifiedBy.hashCode());
         return result;
     }
 
@@ -170,45 +181,10 @@ public class Client implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         Client other = (Client) obj;
-        if (address == null) {
-            if (other.address != null)
-                return false;
-        } else if (!address.equals(other.address))
-            return false;
-        if (cpfCnpj == null) {
-            if (other.cpfCnpj != null)
-                return false;
-        } else if (!cpfCnpj.equals(other.cpfCnpj))
-            return false;
-        if (created == null) {
-            if (other.created != null)
-                return false;
-        } else if (!created.equals(other.created))
-            return false;
-        if (createdBy == null) {
-            if (other.createdBy != null)
-                return false;
-        } else if (!createdBy.equals(other.createdBy))
-            return false;
-        if (fullName == null) {
-            if (other.fullName != null)
-                return false;
-        } else if (!fullName.equals(other.fullName))
-            return false;
         if (id == null) {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
-            return false;
-        if (modified == null) {
-            if (other.modified != null)
-                return false;
-        } else if (!modified.equals(other.modified))
-            return false;
-        if (modifiedBy == null) {
-            if (other.modifiedBy != null)
-                return false;
-        } else if (!modifiedBy.equals(other.modifiedBy))
             return false;
         return true;
     }
--- src/main/java/com/api/mecanica/service/ClientService.java
+++ src/main/java/com/api/mecanica/service/ClientService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.api.mecanica.model.Client;
 import com.api.mecanica.repository.AddressRepository;
 import com.api.mecanica.repository.ClientRepository;
+import com.api.mecanica.repository.PhoneRepository;
 import com.api.mecanica.service.interfaces.ClientServiceInterface;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class ClientService implements ClientServiceInterface {
 
 	@Autowired
 	AddressRepository addressRepository;
+
+	@Autowired
+	PhoneRepository phoneRepository;
 
 	@Override
 	public Client findById(Long id) {
@@ -38,13 +42,26 @@ public class ClientService implements ClientServiceInterface {
 			addressRepository.saveAll(client.getAddress());
 		else
 			System.out.println("Error, endereço obrigatório!");
+
+		if(!client.getPhone().isEmpty())
+			phoneRepository.saveAll(client.getPhone());
+		else
+			System.out.println("Error, phone obrigatório!");
 		Client dbClient = repository.save(client);
 		return dbClient;
 	}
 
 	@Override
 	public Client alterClient(Client client) {
-		addressRepository.saveAll(client.getAddress());
+		if(!client.getAddress().isEmpty())
+			addressRepository.saveAll(client.getAddress());
+		else
+			System.out.println("Error, endereço obrigatório!");
+
+		if(!client.getPhone().isEmpty())
+			phoneRepository.saveAll(client.getPhone());
+		else
+			System.out.println("Error, phone obrigatório!");
 		Client dbClient = repository.save(client);
 		return dbClient;
 	}
